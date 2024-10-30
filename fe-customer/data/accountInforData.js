fdc810d568048075724117f60e8df5eabcd43ff3
Phyo <AungDE150395@fpt.edu.vn>
2024-10-30 22:18:16 +0700

    Initial Commit

---
+++ fe-customer/data/accountInforData.js
@@ -0,0 +1,14 @@
+export const sidebar = [
+    {
+        text: 'Thông tin cá nhân',
+        href: '/account/infor',
+    },
+    {
+        text: 'Danh sách đơn hàng',
+        href: '/account/orders',
+    },
+    {
+        text: 'Thông báo',
+        href: '/account/notification',
+    }
+];
