3437608c9e82ff331837a375b8227c9108a31635
khoadpdde150399 <khoadpdde150399@fpt.edu.vn>
2024-11-26 21:36:46 +0700

    Add catalog+product pages

---
+++ fe-admin/data/data.js
@@ -0,0 +1,25 @@
+export const menu = [
+    
+    {
+        title: 'Category Management',
+        href: '/catalog/manage',
+        list: [
+
+        ],
+    },
+    {
+        title: 'Products Management',
+        href: '/product/manage',
+        list: [
+
+        ],
+    },
+    {
+        title: 'Orders Management',
+        href: '/order/manage',
+        list: [
+
+        ],
+    },
+   
+]
